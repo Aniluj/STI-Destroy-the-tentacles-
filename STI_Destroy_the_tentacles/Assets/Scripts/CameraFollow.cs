de68c33ceef5a0f52039f7b703d491431e9e721d
Aniluj <julianvolonterio@gmail.com>
2017-09-19 06:44:47 -0300

    Commit inicial
    Commit inicial del proyecto:
    
    En este primer commit se incluyen sprites de prueba para las distintas
    funcionalidades que se vayan agregando a medida que se avanza con la
    programación, para sustentar la falta de arte.
    También se implementaron dos scripts, uno para que la camara siga al
    jugador y otro para poder moverse de manera horizontal con la nave.

---
+++ STI_Destroy_the_tentacles/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	private Vector3 posDiff = Vector3.zero;
+
+	void Start () {
+		posDiff = transform.position - target.position;	
+	}
+
+	void LateUpdate () {
+		transform.position = target.position + posDiff;
+	}
+}
---
+++ STI_Destroy_the_tentacles/Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour {
+
+	private float horizontalAxis;
+	private float verticalAxis;
+	private Rigidbody2D spaceship;
+	public float velocity;
+
+	void Awake (){
+		spaceship = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		horizontalAxis = Input.GetAxisRaw ("Horizontal");
+		verticalAxis = Input.GetAxisRaw ("Vertical");
+	}
+
+	void FixedUpdate(){
+		spaceship.MovePosition (transform.position + transform.up * horizontalAxis * Time.fixedDeltaTime * velocity);
+	}
+}
