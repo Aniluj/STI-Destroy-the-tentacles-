8843978a6cc770a49d14208d3df08b1bce15c303
Aniluj <julianvolonterio@gmail.com>
2017-09-26 06:32:06 -0300

    Mecánica de disparo agregada
    Mecánica de disparo agregada, junto a todo lo que esto conlleva: Se
    cuenta con un pool de balas que son desactivadas cuando colisionan y son
    reutilizadas (reactivadas) cuando se necesitan para efectuar un disparo.

---
+++ STI_Destroy_the_tentacles/Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	private Rigidbody2D bullet;
+	public float velocity;
+
+	void Awake (){
+		bullet = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		//transform.Translate (Vector2.right * velocity * Time.deltaTime);
+		bullet.AddRelativeForce (Vector2.right * velocity);
+	}
+
+	void FixedUpdate (){
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D coll){
+		if (coll.gameObject.tag == "Tentacle" || coll.gameObject.name == "Contention shield") {
+			gameObject.SetActive (false);
+		}
+	}
+}
---
+++ STI_Destroy_the_tentacles/Assets/Scripts/Shoot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	private bool firstShoot;
+	private float timer;
+	public float cooldown;
+	public GameObject[] bullet;
+	public Transform shootingPoint;
+
+	void Awake (){
+		
+	}
+
+	void Start () {
+		firstShoot = true;
+	}
+
+	void Update () {
+		timer += Time.deltaTime;
+
+		if (Input.GetKeyDown (KeyCode.Space) && (firstShoot || timer > cooldown)) {
+			timer = 0;
+			firstShoot = false;
+			for(int i = 0; i < 10; i++){
+				if (bullet [i].activeInHierarchy == false) {
+					Debug.Log ("asd");
+					bullet [i].transform.SetPositionAndRotation (shootingPoint.position, shootingPoint.rotation);
+					bullet [i].SetActive (true);
+					break;
+				}
+			}
+			//Instantiate (bullet, shootingPoint.position, shootingPoint.rotation);
+		}
+	}
+
+	void FixedUpdate (){
+		
+	}
+}
