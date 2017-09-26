913ddd3ec7d263d614276ae0249769d6ca08d447
Aniluj <julianvolonterio@gmail.com>
2017-09-26 16:09:18 -0300

    Flow de juego: Menú <-> Gameplay
    Se implementó un flow de juego que permite ir desde un menú muy simple,
    hasta la parte jugable del juego, y viceversa. También se puede
    reiniciar la partida.

---
+++ STI_Destroy_the_tentacles/Assets/Scripts/ChangeSceneAndExit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneAndExit : MonoBehaviour {
+
+	public string sceneName;
+
+	public void Change(){
+		SceneManager.LoadScene (sceneName);
+	}
+
+	public void Exit(){
+		Application.Quit ();
+	}
+}
