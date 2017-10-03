e0871a2be5bce243a060b6930a9c26737c936454
Aniluj <julianvolonterio@gmail.com>
2017-10-03 15:56:16 -0300

    Barra de vida para el escudo, implementada
    Se agregó una barra de vida para el escudo de contencion. La misma
    contiene un funcionamiento similar a la barra de vida de la nave. Recibe
    daño por cada tentáculo que colisiona con el, y cuando la vida llega a
    0, el juego vuelve al menú.

---
+++ STI_Destroy_the_tentacles/Assets/Scripts/ShieldHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ShieldHealth : MonoBehaviour {
+
+	public Slider shieldHealthBar;
+	public int damageReceived;
+	private int initialLife;
+
+	void Start () {
+		initialLife = 100;
+		shieldHealthBar.value = initialLife;
+	}
+	
+	void Update(){
+		if (shieldHealthBar.value <= 0) {
+			SceneManager.LoadScene ("Menu");
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D coll){
+		if (coll.gameObject.tag == "Tentacle") {
+			shieldHealthBar.value -= damageReceived;
+		}
+	}
+}
