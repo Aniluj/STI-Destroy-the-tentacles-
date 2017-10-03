309bb005fc85b767f08b77e56b806f70aaa172eb
Aniluj <julianvolonterio@gmail.com>
2017-10-03 06:56:57 -0300

    Script de TentacleProperties agregado
    Se agregó un script que, actualmente, lo único que hace es detectar
    contra qué colisiona el tentáculo, para que posteriormente se desactive
    el padre del tentáculo y por ende, él también.

---
+++ STI_Destroy_the_tentacles/Assets/Scripts/TentacleProperties.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleProperties : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D coll){
+		if (coll.gameObject.tag == "Shield" || coll.gameObject.tag == "Bullet") {
+			transform.parent.gameObject.SetActive (false);
+		}
+	}
+}
