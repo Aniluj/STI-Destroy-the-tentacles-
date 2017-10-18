2a6120cb52a2c087369aa83376198c0dc51c0149
Aniluj <julianvolonterio@gmail.com>
2017-10-17 22:06:58 -0300

    Spawn de power ups
    Spawn de power ups agregado. Aunque todavía faltan pulir varias cosas, ya que los P.U. no son destruidos y varias veces no salen en la dirección correcta.

--- STI_Destroy_the_tentacles/Assets/Scripts/Power Ups/PowerUpsController.cs
+++ STI_Destroy_the_tentacles/Assets/Scripts/Power Ups/PowerUpsController.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class PowerUpsController : MonoBehaviour {
 
+	public GameObject[] powerUps;
+	public Transform[] spawnPointsForPowerUps;
+	public float timeToSpawnAPowerUp;
+	public float velocity;
+	private bool movementActive;
+	private float timer;
+	private int numberOfSpawn;
+	private int numberOfPowerUp;
 
-		
+	void Start(){
+		movementActive = false;
+	}
+
 	void Update () {
+		timer += Time.deltaTime;
 
+		if (timer >= timeToSpawnAPowerUp) {
+			timer = 0;
+			numberOfPowerUp = Random.Range (0, 3);
+			numberOfSpawn = Random.Range (0, 3);
+			movementActive = true;
+			powerUps [numberOfPowerUp].transform.position = spawnPointsForPowerUps [numberOfSpawn].transform.position;
+			powerUps [numberOfPowerUp].SetActive (true);
+		}
+		if (movementActive) {
+			powerUps [numberOfPowerUp].transform.Translate (Vector3.up * velocity * Time.deltaTime);
+		}
 	  }
    }
