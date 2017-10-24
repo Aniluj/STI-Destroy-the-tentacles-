99f3767a542e3dee6b1d8f16245c6d96cbdf1c42
Aniluj <julianvolonterio@gmail.com>
2017-10-24 06:02:18 -0300

    Sistema del mercado completado
    Ahora todo lo referido al mercado funciona correctamente. Sin embargo, a
    pesar de que los upgrades se pueden comprar, aun no aplican a la
    jugabilidad ya que dichos upgrades no fueron implementados en esta
    versión.

---
+++ STI_Destroy_the_tentacles/Assets/Scripts/Market/MarketController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MarketController : MonoBehaviour {
+
+	public GameObject upgradeOfHealth;
+	public GameObject upgradeOfShieldHealth;
+	public GameObject upgradeOfShot;
+	public Text totalAccumulatedScore;
+	private int upgradeOfHealthIsActive;
+	private int upgradeOfShieldHealthIsActive;
+	private int upgradeOfShotIsActive;
+	private string upgradeOfHealthKey = "upgradeOfHealth";
+	private string upgradeOfShieldHealthKey = "upgradeOfShieldHealth";
+	private string upgradeOfShotKey = "upgradeOfShot";
+	private string accumulatedPointsKey = "accumulatedPoints";
+
+	void Start () {
+		upgradeOfHealthIsActive = PlayerPrefs.GetInt (upgradeOfHealthKey, 1);
+		upgradeOfShotIsActive = PlayerPrefs.GetInt (upgradeOfShotKey, 1);
+		upgradeOfShieldHealthIsActive = PlayerPrefs.GetInt (upgradeOfShieldHealthKey, 1);
+	}
+	
+
+	void Update () {
+		totalAccumulatedScore.text = PlayerPrefs.GetInt (accumulatedPointsKey).ToString ();
+
+		if (upgradeOfHealthIsActive == 0) {
+			upgradeOfHealth.SetActive (false);
+		}
+		if (upgradeOfShotIsActive == 0) {
+			upgradeOfShot.SetActive (false);
+		}
+		if (upgradeOfShieldHealthIsActive == 0) {
+			upgradeOfShieldHealth.SetActive (false);
+		}
+	}
+}
---
+++ STI_Destroy_the_tentacles/Assets/Scripts/Market/MarketFunctionalities.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MarketFunctionalities : MonoBehaviour {
+
+	public int priceOfTheUpgrade;
+	private Text priceText;
+	private string accumulatedPointsKey = "accumulatedPoints";
+	private int totalAccumulatedScore;
+	private string upgradeOfHealthKey = "upgradeOfHealth";
+	private string upgradeOfShieldHealthKey = "upgradeOfShieldHealth";
+	private string upgradeOfShotKey = "upgradeOfShot";
+
+
+	void Awake(){
+		priceText = GetComponentInChildren<Text> ();
+	}
+
+	void Start () {
+		priceText.text = priceOfTheUpgrade.ToString ();
+		totalAccumulatedScore = PlayerPrefs.GetInt (accumulatedPointsKey);
+	}
+
+	public void buyUpgrade(){
+		if (PlayerPrefs.GetInt (accumulatedPointsKey) >= priceOfTheUpgrade) {
+			PlayerPrefs.SetInt (accumulatedPointsKey, totalAccumulatedScore - priceOfTheUpgrade);
+			if (transform.parent.name == "Upgrade of health") {
+				PlayerPrefs.SetInt (upgradeOfHealthKey, 0);
+			}
+			if (transform.parent.name == "Upgrade of shield health") {
+				PlayerPrefs.SetInt (upgradeOfShieldHealthKey, 0);
+			}
+			if (transform.parent.name == "Upgrade of shot") {
+				PlayerPrefs.SetInt (upgradeOfShotKey, 0);
+			}
+			transform.parent.gameObject.SetActive (false);
+		}
+	}
+}
