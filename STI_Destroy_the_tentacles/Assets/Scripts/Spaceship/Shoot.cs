ed323fcc76bc5ec1c5cf8b9fadc22276b9e4dd0b
Aniluj <julianvolonterio@gmail.com>
2017-12-29 20:34:25 -0300

    Script de disparo modificado
    Se modificó el script de disparo para que se haga uso unicamente de uno de estos archivos, y no dedicar uno para cada disparo individual, ya que esto último en ciertas ocasiones causaba efectos no deseados para la jugabilidad.

--- STI_Destroy_the_tentacles/Assets/Scripts/Spaceship/Shoot.cs
+++ STI_Destroy_the_tentacles/Assets/Scripts/Spaceship/Shoot.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour {
 
-	private bool firstShoot;
+	private bool firstShot;
 	private float timer;
 	private string upgradeOfShotKey = "upgradeOfShot";
 	private int upgradeOfShotIsActive;
@@ -12,31 +12,66 @@ public class Shoot : MonoBehaviour {
 	public float cooldown;
 	public GameObject[] bullet;
 	public Transform shootingPoint;
+	public Transform shootingPoint2;
 	public Transform shootingPointOfTheUpgrade;
+	public Transform shootingPointOfTheUpgrade2;
+	public Transform shootingPointOfTheUpgrade3;
 	public bool shooting;
 
 
 
 	void Start () {
-		firstShoot = true;
+		firstShot = true;
 		shooting = false;
 		isOverHeated = false;
 		upgradeOfShotIsActive = PlayerPrefs.GetInt (upgradeOfShotKey);
-		if (upgradeOfShotIsActive == 1) {
-			shootingPoint = shootingPointOfTheUpgrade;
-		}
+		//if (upgradeOfShotIsActive == 1) {
+		//	shootingPoint = shootingPointOfTheUpgrade;
+		//}
 	}
 
 	void Update () {
 		timer += Time.deltaTime;
 
-		if (Input.GetButton ("Fire") && (firstShoot || timer > cooldown) && isOverHeated == false) {
+		if (Input.GetButton ("Fire") && (firstShot || timer > cooldown) && isOverHeated == false) {
 			timer = 0;
-			firstShoot = false;
-			for (int i = 0; i < 20; i++) {
-				if (bullet [i].activeInHierarchy == false) {
-					bullet [i].transform.SetPositionAndRotation (shootingPoint.position, shootingPoint.rotation);
-					bullet [i].SetActive (true);
+			firstShot = false;
+			for (int j = 0; j < 3; j++) {
+				for (int i = 0; i < 30; i++) {
+					if (upgradeOfShotIsActive == 0) {
+						if (bullet [i].activeInHierarchy == false) {
+							if (j == 0) {
+								bullet [i].transform.SetPositionAndRotation (shootingPoint.position, shootingPoint.rotation);
+								bullet [i].SetActive (true);
+								break;
+							}
+							if (j == 1) {
+								bullet [i].transform.SetPositionAndRotation (shootingPoint2.position, shootingPoint2.rotation);
+								bullet [i].SetActive (true);
+								break;
+							}
+						}
+					} else if (upgradeOfShotIsActive == 1) {
+						if (bullet [i].activeInHierarchy == false) {
+							if (j == 0) {
+								bullet [i].transform.SetPositionAndRotation (shootingPointOfTheUpgrade.position, shootingPointOfTheUpgrade.rotation);
+								bullet [i].SetActive (true);
+								break;
+							}
+							if (j == 1) {
+								bullet [i].transform.SetPositionAndRotation (shootingPointOfTheUpgrade2.position, shootingPointOfTheUpgrade2.rotation);
+								bullet [i].SetActive (true);
+								break;
+							}
+							if (j == 2) {
+								bullet [i].transform.SetPositionAndRotation (shootingPointOfTheUpgrade3.position, shootingPointOfTheUpgrade3.rotation);
+								bullet [i].SetActive (true);
+								break;
+							}
+						}
+					}
+				}
+				if (upgradeOfShotIsActive != 1 && j == 1) {
 					break;
 				}
 			}
