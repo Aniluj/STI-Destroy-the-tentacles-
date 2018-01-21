ce1692671e3f2c2976d370b3fd49ce79d4994292
Aniluj <julianvolonterio@gmail.com>
2018-01-21 03:57:15 -0300

    Animación de explosión y cambios en el animator
    Se realizaron algunos cambios en el animator de la nave, en principio para tenerlo más ordenado, y porque debí cambiar los estados de salud de la nave a animaciones individuales, ya que una vez que agregué la animación de explosión de la nave, no pude cambiar los sprites mediante código como hacía en la versión anterior. Por otro lado, aclarar que junto a la animación de la explosión también se agregó un script para hacer vibrar la cámara al momento de que la nave sea destruida.

--- STI_Destroy_the_tentacles/Assets/Scripts/Spaceship/Health.cs
+++ STI_Destroy_the_tentacles/Assets/Scripts/Spaceship/Health.cs
@@ -7,19 +7,24 @@ using UnityEngine.SceneManagement;
 public class Health : MonoBehaviour {
 
 	public Slider healthBar;
+	public Slider overheatSlider;
 	public int damageReceived;
 	public int damageWhenSpecialMovementIsActive;
 	public int damage;
 	public float initialHealth;
-	public Sprite fullHealth;
-	public Sprite halfHealth;
-	public Sprite lowHealth;
+	public GameObject inGameMenu;
+	public CameraShake cameraShakeScript;
 	private int upgradeOfHealthIsActive;
 	private string upgradeOfHealthKey = "upgradeOfHealth";
 	private SpriteRenderer spriteRendererOfTheSpaceship;
+	private Animator spaceshipAnimator;
+	private float timer;
+	private SpaceshipMovement spaceshipMovementScript;
 
 	void Awake(){
+		spaceshipAnimator = GetComponent<Animator> ();
 		spriteRendererOfTheSpaceship = GetComponent<SpriteRenderer> ();
+		spaceshipMovementScript = GetComponent<SpaceshipMovement> ();
 	}
 
 	void Start () {
@@ -32,24 +37,32 @@ public class Health : MonoBehaviour {
 		damageWhenSpecialMovementIsActive = 0;
 		healthBar.maxValue = initialHealth;
 		healthBar.value = initialHealth;
-		spriteRendererOfTheSpaceship.sprite = fullHealth;
 	}
 	
 	void Update(){
 		if (healthBar.value <= 0) {
-			SceneManager.LoadScene ("Market");
+			spaceshipAnimator.SetBool ("IsDead", true);
+			overheatSlider.gameObject.SetActive (false);
+			spaceshipMovementScript.enabled = false;
+			timer += Time.deltaTime;
+			if (timer >= 0 && timer < 0.5) {
+				cameraShakeScript.cameraShakeActivated = true;
+			}
+			if (timer >= 2f) {
+				inGameMenu.SetActive (true);
+				Time.timeScale = 0;
+			}
 		}
-		if (healthBar.value > 0 && initialHealth <= initialHealth / 4) {
-			spriteRendererOfTheSpaceship.sprite = lowHealth;
+		if (healthBar.value > 0 && healthBar.value <= initialHealth / 4) {
+			spaceshipAnimator.SetFloat ("HealthStatus", 1f);
 		}
 		if (healthBar.value > initialHealth/4 && healthBar.value <= initialHealth/2) {
-			spriteRendererOfTheSpaceship.sprite = halfHealth;
+			spaceshipAnimator.SetFloat ("HealthStatus", 0.5f);
 		}
 		if(healthBar.value <= initialHealth && healthBar.value > initialHealth/2){
-			spriteRendererOfTheSpaceship.sprite = fullHealth;
+			spaceshipAnimator.SetFloat ("HealthStatus", 0f);
 		}
 	}
-
 	void OnCollisionEnter2D (Collision2D coll)	{
 		if (coll.gameObject.tag == "Tentacle") {
 			healthBar.value -= damage;
